3a719ea0278537b801710d01a3858b376ca80ab4
Alan Velez <alanvelez20@gmail.com>
2024-03-03 19:40:09 -0600

    Modelo, migracion y controlador

---
+++ database/migrations/2024_03_04_004817_create_animals_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('animals', function (Blueprint $table) {
+            $table->id();
+            $table->string('animal_especie');
+            $table->string('animal_genero');
+            $table->integer('animal_peso');
+            $table->integer('animal_valor_compra');
+            $table->integer('animal_valor_venta');
+            $table->integer('animal_id_lote');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('animals');
+    }
+};
